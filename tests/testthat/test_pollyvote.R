8d3ac3c559a197bfd2a1ea89be3f22cc7973100b
unknown <maierhofert@WAP47.stat.uni-muenchen.de>
2017-05-05 16:39:57 +0200

    test pollyvote object and adding data to a pollyvote object

---
+++ tests/testthat/test_pollyvote.R
@@ -0,0 +1,31 @@
+context("pollyvote")
+
+test_that("pollyvote objects can be created", {
+  # create empty pollyvote container
+  pv = create_pollyvote(id = "test_pv")
+})
+
+test_that("new data can be added to pollyvote objects", {
+  # create empty pollyvote container
+  pv = create_pollyvote(perm_countries = "D")
+  
+  # get data
+  individual.polls = readxl::read_excel(system.file("extdata/German_PollyVote_2013.xlsx", 
+                                                    package = "pollyvoter"), 
+                                        sheet = "Wahlumfrage",
+                                        skip = 1)
+  assert_data_frame(individual.polls, min.cols = 2, min.rows = 2)
+  colnames(individual.polls)[1] = "id"
+  colnames(individual.polls)[3] = "source"
+  colnames(individual.polls) = convert_names(colnames(individual.polls))
+  # sort out empty rows and only the needed columns
+  individual.polls = individual.polls[!is.na(individual.polls$id), 1:11]
+  # coerce to numeric, get NAs
+  individual.polls[,4:11] = apply(individual.polls[,4:11], 2, as.numeric)
+  # coerce to long format
+  ind.polls = wide_to_long_format(individual.polls)
+  
+  # add data to pollyvote
+  expect_error(add_data(pv, newdata = ind.polls, country = "USA"))
+  pv = add_data(pv, newdata = ind.polls, country = "D", election = "BTW")
+})
