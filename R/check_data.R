2b564f4354453a00888010389f4ee509388e7603
maierhofert <thomasmaierhofer@hotmail.de>
2017-05-08 17:29:09 +0200

    add perm_source.types and perm_region.type

--- R/check_data.R
+++ R/check_data.R
@@ -18,18 +18,22 @@ check_data = function(newdata, pv) {
   # check data in colums
   if("country" %in% colnames(newdata) & length(pv$perm_countries) > 0) 
     sapply(newdata$country, assert_choice, pv$perm_countries)
-  if("source" %in% colnames(newdata) & length(pv$perm_sources))
-    sapply(newdata$source, assert_choice, pv$perm_countries)
-  if("election" %in% colnames(newdata) & length(pv$perm_elections)) 
-    sapply(newdata$election, assert_choice, pv$perm_countries)
+  if("source" %in% colnames(newdata) & length(pv$perm_sources) > 0)
+    sapply(newdata$source, assert_choice, pv$perm_sources)
+  if("source.type" %in% colnames(newdata) & length(pv$perm_source.types) > 0)
+    sapply(newdata$source, assert_choice, pv$perm_source.types)
+  if("election" %in% colnames(newdata) & length(pv$perm_elections) > 0) 
+    sapply(newdata$election, assert_choice, pv$perm_elections)
   if("date" %in% colnames(newdata)) {
     stopifnot(all(newdata$date < pv$perm_date_latest))
     stopifnot(all(newdata$date > pv$perm_date_latest))
   }
   if("region" %in% colnames(newdata) & length(pv$perm_regions) > 0) 
-    sapply(newdata$region, assert_choice, pv$perm_countries)
+    sapply(newdata$region, assert_choice, pv$perm_regions)
+  if("region.type" %in% colnames(newdata) & length(pv$perm_region.types) > 0) 
+    sapply(newdata$region, assert_choice, pv$perm_region.types)
   if("party" %in% colnames(newdata) & length(pv$perm_parties) > 0) 
-    sapply(newdata$party, assert_choice, pv$perm_countries)
+    sapply(newdata$party, assert_choice, pv$perm_parties)
   if("percent" %in% colnames(newdata)) 
     assert_numeric(newdata$percent, lower = 0, upper = 100)
   
--- R/create_pollyvote.R
+++ R/create_pollyvote.R
@@ -5,12 +5,10 @@
 #'
 #' @param id [\code{character(1)}]\cr
 #'   the name ID of the pollyvote object, defaults to 'pollyvote'.
-#' @param perm_countries,perm_sources,perm_elections,perm_regions,perm_parties [\code{character(n)}]\cr
+#' @param perm_countries,perm_sources,perm_source.types,perm_elections,perm_regions,perm_region.types,perm_parties [\code{character(n)}]\cr
 #'   permitted values for the respective column of the data of this pollyvote object
 #' @param perm_date_earliest,perm_date_latest [\code{POSIXct(1)}]\cr
 #'   earliest and latest permissible date in format \%Y-\%m-\%d.
-#' @param perm_colnames [\code{character(n)}]\cr
-#'   column names that are permitted in the data of this pollyvote object
 #'
 #' @return object of class pollyvote, containing an id, an initially empty data frame
 #'   and potentially permissible values for the entries of the data frame.
@@ -18,15 +16,15 @@
 #' @export
 create_pollyvote = function(id = "pollyvote",
                             perm_countries = character(0), 
-                            perm_sources = character(0), 
+                            perm_sources = character(0),
+                            perm_source.types = character(0),
                             perm_elections = character(0), 
                             perm_date_earliest = NULL, 
                             perm_date_latest = NULL,
-                            perm_regions = character(0), 
-                            perm_parties = character(0),
-                            perm_colnames = c("id", "country", "source", 
-                                              "election", "date", "region", 
-                                              "party", "percent")) {
+                            perm_regions = character(0),
+                            perm_region.types = character(0),
+                            perm_parties = character(0)
+                            ) {
   # input checking
   assert_character(id)
   assert_character(perm_countries)
@@ -41,28 +39,40 @@ create_pollyvote = function(id = "pollyvote",
     date = format(perm_date_latest, format = "%Y-%m-%d", usetz = FALSE)
   }
   assert_character(perm_regions)
+  assert_character(perm_region.types)
   assert_character(perm_countries)
   assert_character(perm_parties)
-  assert_character(perm_colnames)
+  
   
   # initiate data
   data = data.frame(id = character(), 
                     country = character(), 
                     source = character(), 
+                    source.type = character(),
                     election = character(),
                     date = character(), 
                     region = character(), 
+                    region.type = character(),
                     party = character(),
                     percent = numeric())
+  
+  # initiate permitted col names for data
+  perm_colnames = c("id", "country", 
+                    "source", "source.type",
+                    "election", "date", 
+                    "region", "region.type", 
+                    "party", "percent")
   
   # crate pollyvote object
   pv = list(id = id,
             perm_countries = perm_countries, 
             perm_sources = perm_sources, 
+            perm_source.types = perm_source.types,
             perm_elections = perm_elections, 
             perm_date_earliest = perm_date_earliest, 
             perm_date_latest = perm_date_latest,
             perm_regions = perm_regions, 
+            perm_region.types = perm_region.types,
             perm_parties = perm_parties,
             perm_colnames = perm_colnames,
             data = data)
