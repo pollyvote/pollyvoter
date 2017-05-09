ba2c1036def0e2b8fc8b0785cbcf1a1e5fbccd2e
unknown <maierhofert@WAP48.stat.uni-muenchen.de>
2017-05-09 17:48:54 +0200

    test predict and add_prediction

--- tests/testthat/test_pollyvote.R
+++ tests/testthat/test_pollyvote.R
@@ -12,7 +12,7 @@ test_that("new data can be added to and retrieved from a pollyvote objects", {
   
   # get data
   data("poll_data_long")
- 
+  
   # add data to pollyvote
   expect_error(add_data(pv, newdata = poll_data_long, country = "USA"))
   pv = add_data(pv, newdata = poll_data_long, country = "D", election = "BTW")
@@ -22,7 +22,7 @@ test_that("new data can be added to and retrieved from a pollyvote objects", {
 })
 
 
-test_that("the data from a pollyvote object can be aggregated", {
+test_that("predictions can be added to pollyvote objects", {
   # create empty pollyvote container
   pv = create_pollyvote(perm_countries = "D")
   
@@ -39,23 +39,54 @@ test_that("the data from a pollyvote object can be aggregated", {
   pv = add_data(pv, newdata = poll_data_long, country = "D", region = "national", 
                 source.type = "eco.model", election = "BTW")
   
-  # get the daily aggregated prediction
-  dat_aggr = pv %>% 
-    get_data %>% 
-    filter(source.type %in% c("expert", "poll")) %>%
-    group_by(date, source.type, party) %>% 
-    summarize(aggreg_percent = mean(percent, na.rm = TRUE))
-  dat_aggr
-  # maybe create an aggregated pv object?
+  # add prediction functions to the pollyvote object
+  pv = add_prediction(pv, "expert", function(pv) {
+    pv %>% 
+      get_data %>% 
+      filter(source.type %in% c("expert")) %>%
+      group_by(date, source.type, party) %>% 
+      summarize(percent = mean(percent, na.rm = TRUE))
+  })
+  
+  pv = add_prediction(pv, "poll", function(pv) {
+    pv %>% 
+      get_data %>% 
+      filter(source.type %in% c("poll")) %>%
+      group_by(date, source.type, party) %>% 
+      summarize(percent = mean(percent, na.rm = TRUE))
+  })
+  
+  # check the predictions
+  predict(pv, name = "expert")
+  predict(pv, name = "poll")
   
   
-  # combine the daily predictions with a weighted mean
-  dat_final = dat_aggr %>%
-    mutate(weight = ifelse(source.type == "expert", 1, 2)) %>%
-    group_by(date, party) %>% 
-    summarize(aggreg_percent = weighted.mean(aggreg_percent, weights = weight))
-  dat_final
+  
+  # add a final prediction function
+  pv = add_prediction(pv, "pollyvote", function(pv) {
+    # extract predicted data
+    pred_data = plyr::rbind.fill(predict(pv, "expert"), predict(pv, "poll"))
+    # aggregate it with a weighted mean (random example)
+    dat_final = pred_data %>%
+      mutate(weight = ifelse(source.type == "expert", 1, 2)) %>%
+      group_by(date, party) %>% 
+      summarize(percent = weighted.mean(percent, weights = weight))
+    dat_final
+  })
+  
+  predict(pv, "pollyvote")
+  
+  # pv = add_prediction(pv, "mega_pollyvote", function(pv) {
+  #   # extract predicted data
+  #   pred_data = plyr::rbind.fill(predict(pv, "pollyvote"), predict(pv, "poll"))
+  #   # aggregate it with a weighted mean (random example)
+  #   dat_final = pred_data %>%
+  #     group_by(date, party) %>% 
+  #     summarize(percent = mean(percent))
+  #   dat_final
+  # })
+  # 
+  # predict(pv, "mega_pollyvote")
   
   
 })
-  
