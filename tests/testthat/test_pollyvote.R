e7390299c98d10ccdcd16e6a28faf48428abf71a
unknown <maierhofert@WAP48.stat.uni-muenchen.de>
2017-05-09 19:36:57 +0200

    add test for election result and error calculation

--- tests/testthat/test_pollyvote.R
+++ tests/testthat/test_pollyvote.R
@@ -57,8 +57,8 @@ test_that("predictions can be added to pollyvote objects", {
   })
   
   # check the predictions
-  predict(pv, name = "expert")
-  predict(pv, name = "poll")
+  assert_data_frame(predict(pv, name = "expert"))
+  assert_data_frame(predict(pv, name = "poll"))
   
   
   
@@ -74,7 +74,7 @@ test_that("predictions can be added to pollyvote objects", {
     dat_final
   })
   
-  predict(pv, "pollyvote")
+  assert_data_frame(predict(pv, "pollyvote"))
   
   # pv = add_prediction(pv, "mega_pollyvote", function(pv) {
   #   # extract predicted data
@@ -88,5 +88,73 @@ test_that("predictions can be added to pollyvote objects", {
   # 
   # predict(pv, "mega_pollyvote")
   
+  
+})
+
+test_that("predictions can be added to pollyvote objects", {
+  # create empty pollyvote container
+  pv = create_pollyvote(perm_countries = "D")
+  
+  # get data
+  data("poll_data_long")
+  
+  # add data to pollyvote
+  pv = add_data(pv, newdata = poll_data_long, country = "D", region = "national", 
+                source.type = "poll", election = "BTW")
+  
+  # add prediction functions to the pollyvote object
+  pv = add_prediction(pv, "poll", function(pv) {
+    pv %>% 
+      get_data %>% 
+      filter(source.type %in% c("poll")) %>%
+      group_by(date, source.type, party) %>% 
+      summarize(percent = mean(percent, na.rm = TRUE))
+  })
+  # check the predictions
+  assert_data_frame(predict(pv, name = "poll"))
+  
+  # add an election result
+  data("election_result")
+  pv = add_election_result(pv, "BTW", election_result)
+  # check the get function
+  assert_data_frame(get_election_result(pv, "BTW"))
+})
+
+test_that("the error of pollyvote object can be calculated", {
+  # create empty pollyvote container
+  pv = create_pollyvote(perm_countries = "D")
+  
+  # get data
+  data("poll_data_long")
+  
+  # add data to pollyvote
+  pv = add_data(pv, newdata = poll_data_long, country = "D", region = "national", 
+                source.type = "poll", election = "BTW")
+  
+  # add prediction functions to the pollyvote object
+  pv = add_prediction(pv, "poll", function(pv) {
+    pv %>% 
+      get_data %>% 
+      filter(source.type %in% c("poll")) %>%
+      group_by(date, source.type, party) %>% 
+      summarize(percent = mean(percent, na.rm = TRUE))
+  })
+  
+  # add an election result
+  data("election_result")
+  pv = add_election_result(pv, "BTW", election_result)
+  
+  # add an error calculation function
+  pv = add_error_calc(pv, "poll_only", function(pv) {
+    # extract predicted data
+    pred_data = predict(pv, "poll")
+    # extract election result
+    result = get_election_result(pv, "BTW")
+    joined = left_join(x = pred_data, y = result, by = "party") %>%
+      rename(percent.est = percent.x, percent.true = percent.y)
+    mutate(joined, error = abs(percent.est - percent.true))
+  })
+  
+  assert_data_frame(error_calc(pv, "poll_only"))
   
 })
