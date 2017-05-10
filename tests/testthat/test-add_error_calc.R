bc9f8a78b2a93070456bd726455638f00903955b
unknown <maierhofert@WAP48.stat.uni-muenchen.de>
2017-05-10 12:31:43 +0200

    .

--- tests/testthat/test-add_error_calc.R
+++ tests/testthat/test-add_error_calc.R
@@ -31,8 +31,8 @@ test_that("the error of pollyvote object can be calculated (with CI)", {
     # extract election result
     result = get_election_result(pv, "BTW")
     joined = left_join(x = pred_data, y = result, by = "party") %>%
-      rename(percent.est = percent.x, percent.true = percent.y)
-    return(mutate(joined, error = abs(percent.est - percent.true)))
+      rename(percent = percent.x, percent.true = percent.y)
+    return(mutate(joined, error = abs(percent - percent.true)))
   })
   
   assert_data_frame(error_calc(pv, "poll_only"))
@@ -48,8 +48,8 @@ test_that("the error of pollyvote object can be calculated (with CI)", {
         group_by(party) %>%
         summarize(mean_error = mean(error))
       ec_ci = left_join(error_dat, ec_mean_error, by = "party") %>%
-        mutate(ci_lower = percent.est - qnorm(1-alpha/2) * mean_error,
-               ci_upper = percent.est + qnorm(1-alpha/2) * mean_error)
+        mutate(ci_lower = percent - qnorm(1-alpha/2) * mean_error,
+               ci_upper = percent + qnorm(1-alpha/2) * mean_error)
       return(ec_ci)
     }
     
