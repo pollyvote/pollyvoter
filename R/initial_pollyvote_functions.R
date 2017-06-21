4bb28a2506a4968d4a66c612cc6ca25dccb76a0b
unknown <bortm@WAP47.stat.uni-muenchen.de>
2017-06-21 10:57:25 +0200

    intial aggr_source_type

--- R/initial_pollyvote_functions.R
+++ R/initial_pollyvote_functions.R
@@ -38,3 +38,42 @@ initial_prediction_pollyvote = function(pv, agg_fun = "mean", na_handle = "last"
     group_by(date, party) %>%
     summarize(percent = fun(percent, na.rm = TRUE)) 
 }
+
+
+#' initial aggregated source type prediction
+#' 
+#' TODO
+#' 
+#' @param pv [\code{pollyvote}]\cr
+#'   the pollyvote object of which to get the prediction from.
+#' @param which_source_type [\code{character(1)}]\cr
+#'   the name of the source to use for aggregation 
+#' @param agg_fun [\code{character(1)}]\cr
+#'   the name of the aggregation function to use, currently 'mean' and 'median' are supported
+#'
+#' @return data frame containing the results
+#' 
+#' @inheritParams fill_na
+#' @export
+initial_prediction_aggr_source_type = function(pv, which_source_type, 
+                agg_fun = "mean", 
+                na_handle = "last", ...) {
+  # input checking
+  assert_class(pv, "pollyvote")
+  assert_choice(agg_fun, c("mean", "median"))
+  # evaluate string input
+  fun = switch(agg_fun,
+               mean = mean,
+               median = median)
+  assert_choice(na_handle, c("last", "omit", "mean_within", "mean_across"))
+  if(length(get_perm_source_types(pv)) != 0)
+    lapply(which_source_type, assert_choice, get_perm_source_types(pv))
+  
+  pv %>% 
+    get_data  %>%
+    filter(source_type %in% which_source_type) %>%
+    fill_na(na_handle = na_handle, pv = pv) %>%
+    group_by(date, source_type, party) %>% 
+    summarize(percent = fun(percent, na.rm = TRUE))
+}
+
