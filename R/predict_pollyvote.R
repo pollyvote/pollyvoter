0c522121dab2e47736b3657694a9b2ad49a509cf
maxim199008 <maxim_bort@web.de>
2017-07-17 23:00:46 +0200

    example predict_pollyvote.R

--- R/predict_pollyvote.R
+++ R/predict_pollyvote.R
@@ -6,6 +6,14 @@
 #'   pollyvote object to predict from.
 #' @inheritParams add_prediction
 #' 
+#' @examples
+#' pv = create_pollyvote(perm_countries = "D") 
+#' data("polls_individual")
+#' pv = add_data(pv, newdata = polls_individual, country = "D", region = "national", 
+#'               source_type = "poll", election = "BTW")
+#' pred <- predict(pv, method = "pollyvote")               
+#'               
+#' 
 #' @return a data frame containing the result of the prediction
 #' @family predict
 #'
