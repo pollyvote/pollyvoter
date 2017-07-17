ca5f52ff3856f65ebc7cc0e5cb04d2139d60ad8a
maxim199008 <maxim_bort@web.de>
2017-07-17 22:51:56 +0200

    example error_calc.R

--- R/error_calc.R
+++ R/error_calc.R
@@ -8,6 +8,16 @@
 #'   method name of the error calculation function of \code{object} to use.
 #' @param ... additional arguments to the error calculation function.
 #' 
+#' @examples
+#' pv = create_pollyvote(perm_countries = "D")
+#' data("polls_individual")
+#' pv = add_data(pv, newdata = polls_individual, country = "D", region = "national", 
+#'               source_type = "poll", election = "BTW")
+#' data("election_result")
+#' pv = add_election_result(pv, "BTW 2013", election_result, date = "2013-09-22")
+#' error_calc_pred <- error_calc(pv, "prediction_election", 
+#'                               prediction = "pollyvote", election = "BTW 2013")             
+#' 
 #' @return a data frame containing the result of the error calculation
 #' @family predict
 #'
@@ -21,6 +31,16 @@ error_calc = function(object, method, ...) {
 #' calculate the prediction error of a pollyvote prediction.
 #'
 #' @inheritParams error_calc
+#' 
+#' @examples
+#' pv = create_pollyvote(perm_countries = "D")
+#' data("polls_individual")
+#' pv = add_data(pv, newdata = polls_individual, country = "D", region = "national", 
+#'               source_type = "poll", election = "BTW")
+#' data("election_result")
+#' pv = add_election_result(pv, "BTW 2013", election_result, date = "2013-09-22")
+#' error_calc_pred <- error_calc(pv, "prediction_election", 
+#'                               prediction = "pollyvote", election = "BTW 2013")   
 #' 
 #' @return the prediction
 #' @family predict
