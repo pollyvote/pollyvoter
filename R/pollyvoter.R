33c6fc9d9e829e1ef3bf20f04342d57b97ba288c
maierhofert <thomasmaierhofer@hotmail.de>
2017-07-27 12:18:23 +0200

    update format

--- R/pollyvoter.R
+++ R/pollyvoter.R
@@ -1,7 +1,8 @@
 #' pollyvoter: A package for aggregating polls and calculating forecasts
 #'
 #' The pollyvoter package provides methods for aggregating opinion polls,
-#' calculating forecasts and determining error margins for elections results
+#' calculating forecasts and determining error margins for elections results. 
+#' For an overview of the the workflow check out the vignette (\url{}).
 #'
 #' @import checkmate dplyr tidyr
 #' @docType package
